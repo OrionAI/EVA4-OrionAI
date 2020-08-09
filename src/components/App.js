77b4c362a2275e53d7d8dc7dd35a6a2f6def47aa
Shantanu Acharya <thegeek.004@gmail.com>
2020-08-09 21:16:24 +0530

    Add Footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 
+import Footer from './Footer';
+
 import '../styles/App.css';
 
 const App = () => {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col">
-          <h1 className="heading">Hello</h1>
+    <div className="page-container">
+      <div className="content-wrap">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <h1 className="heading">Hello</h1>
+            </div>
+          </div>
         </div>
       </div>
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import '../styles/Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer mt-5 py-3">
+      <div className="container">
+        <span className="text-muted">Orion AI - EVA4</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
