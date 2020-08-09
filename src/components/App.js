c7395e0439eb119ef309f86fcdcb63c84bbafefc
Shantanu Acharya <thegeek.004@gmail.com>
2020-08-09 21:13:15 +0530

    Boilerplate setup

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import '../styles/App.css';
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1 className="heading">Hello</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
