3e9d88a78e24750e789e0abd20b54d79158cbd4c
Shantanu Acharya <thegeek.004@gmail.com>
2020-08-15 19:55:36 +0530

    add favicon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,16 @@ const Footer = () => {
   return (
     <footer className="footer mt-5 py-3">
       <div className="container">
-        <span className="text-muted">Orion AI - EVA4</span>
+        <div className="row">
+          <div className="col-12 text-left">
+            <img
+              src={`${process.env.PUBLIC_URL}/orionai.png`}
+              style={{ height: '2rem' }}
+              alt="logo"
+            />
+            <span className="text-muted ml-3">Orion AI - EVA4</span>
+          </div>
+        </div>
       </div>
     </footer>
   );
