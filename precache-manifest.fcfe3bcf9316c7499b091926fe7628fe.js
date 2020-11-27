3baf5e2824b08c408e1e201cf7dd3fe519cd568a
Shantanu Acharya <thegeek.004@gmail.com>
2020-11-27 19:04:05 +0530

    Updates

---
+++ precache-manifest.fcfe3bcf9316c7499b091926fe7628fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "846d88f88614e711b9b656a43276608a",
+    "url": "/EVA4-OrionAI/index.html"
+  },
+  {
+    "revision": "2a8e78e89a7bc3059762",
+    "url": "/EVA4-OrionAI/static/css/main.680578b7.chunk.css"
+  },
+  {
+    "revision": "b890ad6d9ff251145c61",
+    "url": "/EVA4-OrionAI/static/js/2.a052143a.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/EVA4-OrionAI/static/js/2.a052143a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a8e78e89a7bc3059762",
+    "url": "/EVA4-OrionAI/static/js/main.952aca5c.chunk.js"
+  },
+  {
+    "revision": "f4c50e4287e04d60b179",
+    "url": "/EVA4-OrionAI/static/js/runtime-main.389382f2.js"
+  }
+]);
