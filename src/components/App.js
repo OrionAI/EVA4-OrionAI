604829024d57703e6e7ecc59fe820486cc4a8eda
Shantanu Acharya <thegeek.004@gmail.com>
2022-07-02 19:39:52 -0400

    add backend shutdown alert to the top of the page

--- src/components/App.js
+++ src/components/App.js
@@ -111,6 +111,31 @@ class App extends React.Component {
     };
   }
 
+  renderBackendShutdownAlert() {
+    return (
+      <div class="alert alert-success mt-5" role="alert">
+        <h4 class="alert-heading">Backend Shutdown!</h4>
+        <p>
+          In order to save server costs, all the backend services for this
+          portal have been shutdown. This means that none of the models in this
+          portal will give any output.
+        </p>
+        <hr />
+        <p class="mb-0">
+          To get the backend functionality back, please refer to the{' '}
+          <a
+            href="https://github.com/OrionAI/EVA4-Phase-2"
+            className="alert-link"
+          >
+            EVA4-Phase-2
+          </a>{' '}
+          GitHub repository for instructions to setup your own backend servers
+          on AWS.
+        </p>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className="page-container">
@@ -118,6 +143,7 @@ class App extends React.Component {
           <Router history={history}>
             <Navbar componentItems={this.componentItems} />
             <div className="container">
+              {this.renderBackendShutdownAlert()}
               <Switch>
                 <Route
                   path="/"
